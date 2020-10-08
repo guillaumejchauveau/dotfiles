3899a0ca24c215530d663aa70055327744b6ae5d
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2020-10-08 23:57:42 +0200

    P10K

---
+++ .oh-my-zsh-custom/colors.zsh
@@ -0,0 +1,17 @@
+# Colors
+echo -en "\e]P0000000" #black
+echo -en "\e]P1E54B4B" #red
+echo -en "\e]P2C3E88D" #green
+echo -en "\e]P3FAED70" #yellow
+echo -en "\e]P482AAFF" #blue
+echo -en "\e]P5C792EA" #magenta
+echo -en "\e]P689DDFF" #cyan
+echo -en "\e]P7d0d0d0" #white
+echo -en "\e]P85a5a5a" #bold-black
+echo -en "\e]P9FF5370" #bold-red
+echo -en "\e]PA9ECE58" #bold-green
+echo -en "\e]PBFFCB6B" #bold-yellow
+echo -en "\e]PC396FE2" #bold-blue
+echo -en "\e]PDBB80B3" #bold-magenta
+echo -en "\e]PE2DDAFD" #bold-cyan
+echo -en "\e]PFffffff" #bold-white
