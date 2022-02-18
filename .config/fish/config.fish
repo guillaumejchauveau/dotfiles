1a3521a852038876188e3138db91aceabec86a87
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2022-02-19 00:33:15 +0100

    Migrate to fish, starship prompt

---
+++ .config/fish/config.fish
@@ -0,0 +1,50 @@
+fish_add_path $HOME/.garden/bin
+set fish_greeting
+
+if status is-interactive
+  if test -x /usr/bin/dircolors
+    if test -r ~/.dir_colors
+      dircolors -c ~/.dir_colors | sed 's/setenv/set/' | source
+    else if test -r /etc/dir_colors
+      dircolors -c /etc/dir_colors | sed 's/setenv/set/' | source
+    else
+      dircolors -c | sed 's/setenv/set/' | source
+    end
+  end
+
+  abbr -a -g ls 'ls --color=auto'
+  abbr -a -g l 'ls -lahG'
+  abbr -a -g la 'ls -lAhG'
+  abbr -a -g ll 'ls -lhG'
+  abbr -a -g lsa 'ls -lah'
+  abbr -a -g grep 'grep --color=auto'
+  abbr -a -g fgrep 'fgrep --color=auto'
+  abbr -a -g egrep 'egrep --color=auto'
+
+  set fish_color_normal normal
+  set fish_color_command green
+  set fish_color_keyword green
+  set fish_color_quote yellow
+  set fish_color_redirection magenta
+  set fish_color_end brmagenta
+  set fish_color_error brred
+  set fish_color_param normal
+  set fish_color_comment brblack
+  set fish_color_selection --background=brblack
+  set fish_color_operator cyan
+  set fish_color_escape --bold
+  set fish_color_autosuggestion brblack
+  set fish_color_cwd blue
+  set fish_color_cwd_root red
+  set fish_color_user brblack
+  set fish_color_host brblack
+  set fish_color_host_remote brblack
+  set fish_color_cancel red
+  set fish_color_valid_path blue
+  set fish_color_history_current --bold
+  set fish_color_match bryellow --background=brblack
+  set fish_color_status red
+  set fish_color_search_match bryellow --background=brblack
+
+  starship init fish | source
+end
